81e94198ec47319e1a35527a72944e778ffcb237
Adi Cohen <Adi.cohen5@amdocs.com>
2020-01-04 11:23:36 +0200

    4/1/2020 v2

--- GameLogic.py
+++ GameLogic.py
@@ -3,34 +3,12 @@ def checkHands(player1Hands, player2Hands, deckCards):
         flowOfChecks(player1Hands[x])
 
 
-def sortByRank(hand):
-    for i in hand:
-        'a'
-
-
-
 def flowOfChecks(hand):
-    orderedByRankHand = sorted(hand, key = lambda x: x.getRank(), reverse =True)
-    orderedBySuitHand = sorted(hand, key = lambda x: x.getSuit(), reverse =True)
+    orderedByRankHand = sorted(hand, key=lambda x: x.getRank(), reverse=True)
+    orderedBySuitHand = sorted(hand, key=lambda x: x.getSuit(), reverse=True)
 
     isRoyal(orderedByRankHand)
 
 
 def isRoyal(hand):
-    sortedHand = sorted(hand, reverse=True)
-    flag = True
-    h = 10
-    Cursuit = sortedHand[0].suit
-    Currank = 14
-    total_point = h * 13 ** 5 + self.point(sortedHand)
-    for card in sortedHand:
-        if card.suit != Cursuit or card.rank != Currank:
-            flag = False
-            break
-        else:
-            Currank -= 1
-    if flag:
-        print('Royal Flush')
-        self.tlist.append(total_point)
-    else:
-        self.isStraightFlush(sortedHand)
+    'a'
